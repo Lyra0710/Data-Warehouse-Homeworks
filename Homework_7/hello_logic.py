3cfee273b97de6b9db0eea2f6ae162045fc7328a
Lyra <ananyayallapragada@gmail.com>
2026-03-18 23:24:28 -0700

    Homework 7 Files

---
+++ Homework_7/hello_logic.py
@@ -0,0 +1,6 @@
+def print_hello():
+    print("hello! - 2026.02")
+    return "hello!"
+def print_goodbye():
+    print("goodbye!")
+    return "goodbye!"
---
+++ Homework_7/print_goodbye.py
@@ -0,0 +1,4 @@
+from hello_logic import print_goodbye
+
+if __name__ == "__main__":
+     print(print_goodbye(), "- Ananya Yallapragada")
